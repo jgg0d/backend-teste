7e106679f13bc5299a9320733e3825570993f9d5
jgg_git <49101191+jgg0d@users.noreply.github.com>
2024-06-19 22:41:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ app.get("/clients", (req, res) => {
   res.status(200).json([
     {
       nome: "João",
-      data_nascimento: "01/01/2002",
-      cpf: "111-222-333-98",
-      endereco: "Rua 1, centro, SP, CEP: 13444-002",
+      data_nascimento: "2002-02-02",
+      cpf: "11122233398",
+      endereco: "Av. Avenida Brasil Brasil Brasil BrasilBrasil, centro, SP, CEP: 13444-002",
       status: "Ativo",
     },
     {
       nome: "Maria",
-      data_nascimento: "01/01/2001",
-      cpf: "444-222-333-98",
+      data_nascimento: "2001-01-01",
+      cpf: "44422233398",
       endereco: "Rua 2, centro, SP, CEP: 13999-001",
       status: "Inativo",
     },
